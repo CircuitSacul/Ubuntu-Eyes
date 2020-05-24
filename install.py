7f320c3429085cc788159e2101dc928971f3e11d
CircuitSacul <circuitsacul@gmail.com>
2020-05-24 13:50:12 -0400

    Var COMMAND changed to COMMANDS to support multiple commands

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 import os
 
 OLD_COMMANDS = []
-COMMAND = '@reboot python3 /bin/UbuntuEyes/main.py &'
+COMMANDS = ['@reboot python3 /bin/UbuntuEyes/main.py &']
 
 
 os.system('git pull')
@@ -13,10 +13,12 @@ os.system('sudo cp -r UbuntuEyes /bin')
 os.system('crontab -l > tmpcron')
 with open('tmpcron', 'r') as f:
     contents = f.read()
-    if COMMAND not in contents:
-        os.system(f'echo "{COMMAND}" >> tmpcron')
-        os.system('crontab tmpcron')
-    else:
+    for command in COMMANDS:
+        if command not in contents:
+            os.system(f'echo "{command}" >> tmpcron')
+            os.system('crontab tmpcron')
+        else:
+            pass
     for command in OLD_COMMANDS:
         if command in contents:
             contents.replace(command, '')
